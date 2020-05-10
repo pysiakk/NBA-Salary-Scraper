8acfa600c7f87f04545fad66005a956b539b1f13
Karol Pysiak <karol.pysiak.13@gmail.com>
2020-05-10 14:11:48 +0200

    Complete code upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # NBA-Salary-Scraper
 This repo contains a script for scraping NBA players' salaries from ESPN page.
+
+## Usage
+
+`
+salary_scrape(year)
+`
+
+## Ouput
+
+`Pandas.DataFrame` containing a position in the salary ranking, the name of a player, the team of a player and his salary in dollars.
+
+
+## Examples
+
+`
+sal = salary_scrape(2020)
+print(sal.iloc[0])
+`
+
+`
+RK                            1
+NAME              Stephen Curry
+TEAM      Golden State Warriors
+SALARY                 40231758
+Name: 0, dtype: object
+`
+
---
+++ nbasalaryscraper.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+def salary_scrape(year):
+    src = "http://www.espn.com/nba/salaries/_/year/" + str(year) + "/page/"
+    data = None
+    for k in range(1, 20):
+        url = src + str(k)
+        html = urlopen(url)
+        soup = BeautifulSoup(html, features="html.parser")
+        rows = soup.findAll('tr')[1:]
+        player_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
+        stats = pd.DataFrame(player_stats, columns=[th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('td')])
+        stats = stats[stats.RK != "RK"].reset_index(drop=True)
+        for i, name in enumerate(stats.NAME):
+            stats.NAME[i] = re.sub(",\s.*", "", name)
+        for i, salary in enumerate(stats.SALARY):
+            stats.SALARY[i] = int(re.sub("\$|,", "", salary))
+        if k == 1:
+            data = stats
+        else:
+            data = pd.concat([data, stats])
+    return data.reset_index(drop=True)
